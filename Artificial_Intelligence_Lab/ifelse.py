476b8a89d4315c3060f514e314a1c3bc7a8a63cd
saniya <saniyakorabu612@gmail.com>
2025-07-09 14:32:25 +0530

    this is an if else code

---
+++ Artificial_Intelligence_Lab/ifelse.py
@@ -0,0 +1,10 @@
+num=float(input("enter the number:"))
+ 
+ if num>0:
+    print("the number is positive")
+    
+ elif num<0:
+     print("the number is neagtive")
+ 
+ else:
+     print("the number is zero")
